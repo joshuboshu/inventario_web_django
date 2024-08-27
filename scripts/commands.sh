91840fabd082093a5905416da3f04dd48b67a8e8
Joshua Colman <163332471+joshuboshu@users.noreply.github.com>
2024-08-27 12:25:10 -0400

    Update commands.sh

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -9,6 +9,10 @@ done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
+# Ajustar permisos en los directorios estáticos y de medios
+chmod -R 755 /data/web/static /data/web/media
+
+# Recolectar archivos estáticos, hacer migraciones e iniciar el servidor
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
